934ff41453eca016a19cb044dbc39d8da6e646cb
Kunal Bhat <kunal.bhat2001@gmail.com>
2022-04-27 08:33:20 +0530

    Initial Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,8 @@
 package com.company;
-import javax.swing.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        JButton button = new JButton("Click Here");
-        button.setBounds(130,100,100,40);
-        frame.add(button);
-        frame.setSize(500,500);
-        frame.setLayout(null);
-        frame.setVisible(true);
+
     }
 }
---
+++ src/com/company/view/UI.java
@@ -0,0 +1,5 @@
+package com.company.view;
+import javax.swing.*;
+
+public class UI {
+}
